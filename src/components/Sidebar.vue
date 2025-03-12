c548d70b73d6739b83fb539ac9d027e63dfae564
FrankVan1982 <consulenza.vangi@outlook.com>
2025-03-12 19:04:42 +0100

    Update (there were errors)

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -29,7 +29,7 @@ const mode = useColorMode({
   disableTransition: false,
 });
 const isLoggedIn = ref(false);
-let auth;
+const auth = getAuth();
 onMounted(() => {
   auth = getAuth();
   onAuthStateChanged(auth, (user) => {
