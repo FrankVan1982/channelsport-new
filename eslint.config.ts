436ac038cf9cfe9fe47802619d86905a6041bc62
FrankVan1982 <consulenza.vangi@outlook.com>
2025-03-12 19:29:41 +0100

    Updates to various files

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,5 @@
 import pluginVue from 'eslint-plugin-vue'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
 import oxlint from 'eslint-plugin-oxlint'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
@@ -22,7 +21,7 @@ export default defineConfigWithVueTs(
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
