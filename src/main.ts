da6096ee1b86b8df8a1c46eacdb20c7c12c71341
FrankVan1982 <consulenza.vangi@outlook.com>
2025-03-13 16:48:40 +0100

    index.js

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index'
+import router from './router'
 import { initializeApp } from 'firebase/app'
 import '@fontsource-variable/manrope'
 
